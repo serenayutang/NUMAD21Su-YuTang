f6a5516f7ade4a54bcfaa9070458b6071e02910a
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-05-16 20:27:01 -0700

    add about button

--- app/src/main/java/neu/madcourse/numad21su_yutang/MainActivity.java
+++ app/src/main/java/neu/madcourse/numad21su_yutang/MainActivity.java
@@ -3,6 +3,9 @@ package neu.madcourse.numad21su_yutang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutBtn = (Button) findViewById(R.id.aboutBtn);
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Name: Yu Tang;\nEmail: tang.y@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
