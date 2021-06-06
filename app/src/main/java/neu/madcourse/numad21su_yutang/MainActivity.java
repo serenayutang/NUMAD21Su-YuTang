d0d461d3c7938dcc72c0a119ba154ab71df21077
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-06-06 14:27:19 -0700

    update main activity and manifest

--- app/src/main/java/neu/madcourse/numad21su_yutang/MainActivity.java
+++ app/src/main/java/neu/madcourse/numad21su_yutang/MainActivity.java
@@ -1,11 +1,10 @@
 package neu.madcourse.numad21su_yutang;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,19 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button aboutBtn = (Button) findViewById(R.id.aboutBtn);
+        Button aboutBtn = findViewById(R.id.aboutBtn);
         aboutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Name: Yu Tang;\nEmail: tang.y@northeastern.edu", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),
+                        "Name: Yu Tang;\nEmail: tang.y@northeastern.edu",
+                        Toast.LENGTH_LONG).show();
             }
         });
-
-        Button launcherBtn = (Button) findViewById(R.id.launcher);
+        Button launcherBtn = findViewById(R.id.launcher);
         launcherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSixBtns();
+            }
+        });
+
+        Button linkCollector = findViewById(R.id.linkCollector);
+        linkCollector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent linkCollectorIntent = new Intent(getApplicationContext(),
+                        LinkCollectorActivity.class);
+                startActivity(linkCollectorIntent);
             }
         });
     }
